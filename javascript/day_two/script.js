4fdeead4df9bde1135332c3593fc0326785720b2
Michał Kozak <kozak_m@outlook.com>
2018-04-08 11:04:01 +0200

    Update script.js

--- javascript/day_two/script.js
+++ javascript/day_two/script.js
@@ -36,3 +36,35 @@ switch(five){
     default:
         console.log('Zmienna jest różna od 1 i 2');
 }
+
+//Task 6
+
+var numbers = [1,2,3,4,5];
+
+for (var i = 0; i <= 5; i++){
+    console.log(numbers[i]);
+}
+
+//Task 7
+
+var random
+var smallerThanHalf = 0
+
+for (var i = 0; i<10; i++) {
+    random = Math.random();
+    console.log('random', i, '=', random)
+
+    if (random < 0.5) {
+        smallerThanHalf++
+    }
+}
+
+//Task 8
+
+var milkTemp = 20;
+
+while(milkTemp<=100){
+    milkTemp += Math.random()
+}
+
+console.log('Temperature', milkTemp);
