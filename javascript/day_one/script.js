36e429dcfa974a3cbbf72df5ff725df717e5300d
Michał Kozak <kozak_m@outlook.com>
2018-04-11 10:06:49 +0200

    Update script.js

--- javascript/day_one/script.js
+++ javascript/day_one/script.js
@@ -1,15 +1,26 @@
 //Task 1
 
-alert("hello world");
+var personWife = null
+var personDog = { name: 'Cat' }
+var zipCode = '21-040'
+var hoursInDay = 24;
+var days = [1, 15, 30]
 
-console.log(typeof + 342342);
-console.log([1,2,3,4]);
+console.log('personWife', personWife, typeof personWife)
+console.log('personDog', typeof personDog)
+console.log('zipCode', typeof zipCode)
+console.log('hoursInDay', typeof hoursInDay)
+console.log('days', typeof days)
 
 //Task 2
 
-var name = "Michal";
-var age = 25;
+var ageSum = 20 + 24 + 25 + 30 + 35 + 40
+var peoplesInClass = 6
+var avg = ageSum / peoplesInClass;
 
-console.log(typeof + name);
+console.log('sum', ageSum)
+console.log('avg', avg)
 
-console.log("My name is " + name);
+if (ageSum % 2 == 0) {
+    console.log('sum is even')
+}
