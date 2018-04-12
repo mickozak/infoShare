b9bdb312a77e86eaa5b55a5e62550b3d94b9bb81
Michał Kozak <kozak_m@outlook.com>
2018-04-12 14:53:24 +0200

    Add files via upload

---
+++ javascript/day_one/TASK 1.5/script.js
@@ -0,0 +1,12 @@
+var ageSum = 20 + 24 + 25 + 30 + 35 + 40
+var peoplesInClass = 6
+
+var avg = ageSum / peoplesInClass;
+
+console.log('sum', ageSum)
+console.log('avg', avg)
+
+// check sum is even
+if (ageSum % 2 == 0) {
+    console.log('sum is even')
+}
