5953a4cef97a9524ef694b331abc4b716e2e65f1
Michał Kozak <kozak_m@outlook.com>
2018-04-14 14:02:46 +0200

    Update script.js

--- javascript/day_two/TASK 2.4/script.js
+++ javascript/day_two/TASK 2.4/script.js
@@ -1,11 +1,11 @@
-var value = 2 // variable to check
+var value = 2 
 
 var ternaryResult = value === 1
-    ? 1 /* return 1 if value === 1 */
+    ? 1 
     : value === '1'
-        ? 2  /* return 2 if value === '1' */
+        ? 2  
         :  value === 2
-            ? 3 /* return 3 if value === 2 */
-            : 0  /* default: return 0 */
+            ? 3 
+            : 0  
 
 console.log(ternaryResult)
