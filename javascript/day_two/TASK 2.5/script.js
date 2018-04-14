25c79de6f891e50e425f60c9e3373b4931151414
Michał Kozak <kozak_m@outlook.com>
2018-04-14 14:03:06 +0200

    Update script.js

--- javascript/day_two/TASK 2.5/script.js
+++ javascript/day_two/TASK 2.5/script.js
@@ -2,10 +2,10 @@ var random
 var smallerThanHalf = 0;
 
 for(var i = 0; i < 10; i++) {
-    random = Math.random() // generate random number and assign to "random" variable
+    random = Math.random()
     console.log('random', i, ' = ', random)
 
-    if (random < 0.5) { // increase smallerThanHalf variable if random is smaller than 0.5
+    if (random < 0.5) { 
         smallerThanHalf++
     }
 }
