a156c656c8e54c831ee014136cf17417e9319494
Michał Kozak <kozak_m@outlook.com>
2018-04-14 14:08:44 +0200

    Create script.js

---
+++ javascript/day_three/TASK 3.8/script.js
@@ -0,0 +1,14 @@
+var myCar = {
+    make: 'Opel',
+    model: 'Corsa C',
+    year: 2004,
+    color: 'yellow'
+}
+
+function changeColor(car, newColor) {
+    car.color = newColor
+}
+
+changeColor(myCar, 'blue')
+
+console.log(myCar)
